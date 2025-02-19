14bb1a5d01ab211ef4bccf6a0c66b015ad421f97
khandaremanisha9 <khandaremanisha9@gmail.com>
2025-02-19 18:20:38 +0530

    Create RWOnly.java

---
+++ RWOnly.java
@@ -0,0 +1,12 @@
+public class RWOnly {
+    private String name; // private member variable
+
+    // Task 5: Implement getter and setter methods
+    public String getName() {
+        return name; // Getter for `name`
+    }
+
+    public void setName(String name) {
+        this.name = name; // Setter for `name`
+    }
+}
